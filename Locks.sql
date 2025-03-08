c6645fc183b3bdebea33c6822f5fdd7d03f1812f
lomaxc <lomaxc@lomaxdigital.com>
2025-03-07 22:35:26 -0500

    Add initial Locks.sql file with lock modes and isolation levels documentation

---
+++ Locks.sql
@@ -0,0 +1,31 @@
+/*
+
+    Locks:
+    - RID/Key/Row
+    - Page
+    - Extent
+    - HoBT/Object
+    - Application
+    - Metadata
+    - Allocation Unit
+    - Database
+
+    Lock Modes:
+    - Shared
+    - Update
+    - Intent Exclusive
+    - Exclusive
+    - Schema Stability
+    - Schema Modification
+    - Bulk Update
+    - Key-Range
+
+    Isolation Levels:
+    - Read Uncommitted/NOLOCK
+    - Read Committed
+    - Read Committed Snapshot
+    - Snapshot
+    - Repeatable Read
+    - Serializable
+    
+*/
